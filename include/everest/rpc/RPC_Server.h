5352f7bd15beee0018f12d642a2cb0711140801b
luckysym <powdog@yeah.net>
2017-12-09 02:52:35 +0800

    封装和实现EPoller::wait

--- include/everest/rpc/RPC_Server.h
+++ include/everest/rpc/RPC_Server.h
@@ -210,10 +210,14 @@ namespace net
     {
     public:
         static const int Event_Read = EPOLLIN;
-    
+        
+        static const int Step_Size = 1024;
     private:
-        int m_epfd;
-    
+        int           m_epfd;
+        int           m_maxevents;
+        int           m_eventcount;
+        epoll_event * m_pevents;
+        
     private:
         EPoller(const EPoller& ) = delete;
         EPoller& operator=(const EPoller&) = delete;
@@ -225,16 +229,20 @@ namespace net
         bool add(int fd, int events, void * pdata);
         bool remove(int fd);
         bool set(int fd, int events, void *pdata);
+        int  wait(int timeout);
         
     }; // end of class EPoller
     
     
-    inline EPoller::EPoller() 
+    inline EPoller::EPoller() : m_maxevents(0), m_eventcount(0)
     {
         m_epfd = ::epoll_create1(EPOLL_CLOEXEC);
         if ( m_epfd < 0 ) {
             printf("[ERROR] EPoller::EPoller, epoll_create1 failed, %d, %s\n", errno, strerror(errno));
         }
+        m_pevents = (epoll_event *)malloc(Step_Size * sizeof(epoll_event));
+        m_maxevents = Step_Size;
+        assert(m_pevents);
     }
     
     inline EPoller::~EPoller()
@@ -243,6 +251,10 @@ namespace net
             ::close(m_epfd);
             m_epfd = -1;
         }
+        free(m_pevents);
+        m_pevents = nullptr;
+        m_eventcount = 0;
+        m_maxevents = 0;
     }
     
     inline bool EPoller::add(int fd, int events, void *pdata)
@@ -254,6 +266,13 @@ namespace net
         if ( ret < 0 ) {
             printf("[ERROR] EPoller::add, epoll_ctl failed, %d, %s\n", errno, strerror(errno));
             return false;
+        }
+        m_eventcount += 1;
+        if ( m_eventcount > m_maxevents ) {
+            epoll_event * pevents = (epoll_event*)::realloc(m_pevents, (m_maxevents + Step_Size) * sizeof(epoll_event));
+            assert(pevents);
+            m_pevents = pevents;
+            m_maxevents += Step_Size;
         }
         return true;
     }
@@ -279,6 +298,16 @@ namespace net
             return false;
         }
         return true;
+    }
+    
+    inline int EPoller::wait(int timeout) 
+    {
+        int ret = ::epoll_wait(m_epfd, m_pevents, m_maxevents, timeout);
+        if ( ret < 0 ) {
+            printf("[ERROR] EPoller::wait, epoll_wait fail, %d, %s\n", errno, strerror(errno));
+            return false;
+        }
+        return ret;
     }
     
 } // end of namespace net 
